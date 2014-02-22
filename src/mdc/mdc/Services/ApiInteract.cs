78d0cef4cbda05be4b64e4ddf7868527ff1488fb
Kaelan Fouwels <kfouwels@outlook.com>
2014-02-22 11:59:27 +0000

    json templates + api mock requester

--- src/mdc/mdc/Services/ApiInteract.cs
+++ src/mdc/mdc/Services/ApiInteract.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using mdc.Templates;
 
-namespace Findler.Services
+namespace mdc.Services
 {
     internal class ApiInteract
     {
+	    public Task<mdc.Templates.SummaryReturn.Root> GetSummary(string company)
+	    {
+		    //HttpGet(*url*)
+			throw new NotImplementedException();
+	    }
+
         private async Task<string> HttpGet(string urlIn)
         {
             var request = (HttpWebRequest) WebRequest.Create(urlIn);
---
+++ src/mdc/mdc/Templates/SummaryReturn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mdc.Templates
+{
+	public class SummaryReturn
+	{
+		public class NewsSource
+		{
+			public string name { get; set; }
+			public string headline { get; set; }
+			public double polarity { get; set; }
+		}
+
+		public class Root
+		{
+			public string company_name { get; set; }
+			public string mission_statement { get; set; }
+			public string mission_statement_proof { get; set; }
+			public string mission_statement_investigator { get; set; }
+			public List<NewsSource> news_sources { get; set; }
+		}
+	}
+}
