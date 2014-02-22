418f1655ad2acfd3252d65c618c94591c18559f3
Kaelan Fouwels <kfouwels@outlook.com>
2014-02-22 11:49:20 +0000

    Added proj + templates

---
+++ src/mdc/mdc/Pages/CrunchPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace mdc.Pages
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class CrunchPage : Page
+	{
+		public CrunchPage()
+		{
+			this.InitializeComponent();
+		}
+
+		protected async override void OnNavigatedTo(NavigationEventArgs e)
+		{
+			StatusMessage.Text = "Do data stuff here";
+			TheProgressRing.IsActive = true;
+			//Do stuff
+
+			await Task.Delay(2000);
+
+			TheProgressRing.IsActive = false;
+			var fr = new Frame();
+			fr.Navigate(typeof(MainPage));
+			Window.Current.Content = fr;
+			Window.Current.Activate();
+		}
+	}
+}
