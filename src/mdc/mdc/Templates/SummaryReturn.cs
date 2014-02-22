fd1997a2f059861460347100320acc9857b06740
Kaelan Fouwels <kfouwels@outlook.com>
2014-02-22 20:56:39 +0000

    new json and int dont awntt od topjwo;irsljkngfxcm .,s\mf,

--- src/mdc/mdc/Templates/SummaryReturn.cs
+++ src/mdc/mdc/Templates/SummaryReturn.cs
@@ -10,17 +10,20 @@ namespace mdc.Templates
 	{
 		public class NewsSource
 		{
+			public int id { get; set; }
+			public int company_id { get; set; }
 			public string name { get; set; }
 			public string headline { get; set; }
 			public double polarity { get; set; }
+			public string url { get; set; }
 		}
 
 		public class Root
 		{
 			public string name { get; set; }
 			public string mission_statement { get; set; }
-			public string mission_statement_proof { get; set; }
-			public string mission_statement_investigator { get; set; }
+			public object mission_statement_proof { get; set; }
+			public object mission_statement_investigator { get; set; }
 			public List<NewsSource> news_sources { get; set; }
 		}
 	}
