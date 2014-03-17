397e015b3ca4b4bf667011127e55b293a5afc722
Kaelan Fouwels <kfouwels@outlook.com>
2014-03-17 18:29:03 +0000

    linq2twitter

---
+++ src/mdc/mdc/Services/TwitterInteract.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqToTwitter;
+
+namespace mdc.Services
+{
+	class TwitterInteract
+	{
+		async void GetTweets(string query, IAuthorizer auth) //lol oauth
+		{
+			var twitterCtx = new TwitterContext(auth);
+			var searchResults =
+				twitterCtx.Search.Where(search => search.Type == SearchType.Search
+				                                  && search.Query == query);
+
+			var searched = searchResults.SingleOrDefault();
+		}
+	}
+}
